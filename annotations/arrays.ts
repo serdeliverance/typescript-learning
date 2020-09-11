a46448bd803c24725df3acc85c5731ab3688d414
Sergio Cano <serdeliverance@gmail.com>
2020-09-11 18:38:21 -0300

    why typed arrays

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -8,3 +8,16 @@ const dates = [new Date(), new Date()]
 
 // a two dimension array
 const carsByMake = [['f150'], ['corolla'], ['camaro']]
+
+// why typed arrays
+
+// 1) help with inference when extracting values
+const car = carMarkers[0]
+
+// 2) prevent incompatible values
+// carMarkers.push(100)      // typescript tell us that we are trying to push an invalid value
+
+// 3) Help with 'map', 'foreach' and 'reduce'
+carMarkers.map((car: string): string => {
+  return car // because we indicate the type, typescript help us with method autocompletion
+})
