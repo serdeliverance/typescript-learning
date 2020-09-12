70151150a67647f9a4335d40da32faef0d633f06
Sergio Cano <serdeliverance@gmail.com>
2020-09-12 20:10:38 -0300

    modifier and classes syntatic sugar

--- classes.ts
+++ classes.ts
@@ -1,11 +1,43 @@
 class Vehicle {
+  color: string
+
+  constructor(color: string) {
+    this.color = color
+  }
+
   drive(): void {
     console.log('driving...')
   }
+
+  honk(): void {
+    console.log('bump!')
+  }
 }
 
-const vehicle = new Vehicle()
+const vehicle = new Vehicle('red')
 vehicle.drive()
 
 // inheritance
 class Car extends Vehicle {}
+
+// modifiers
+
+// public
+
+// private: only accessable inside the class.
+
+// protected: can only be accessed inside the class or by subclass.
+
+// syntactic sugar for constructors
+
+class Vehicle2 {
+  constructor(public color: string) {}
+
+  drive(): void {
+    console.log('driving...')
+  }
+
+  honk(): void {
+    console.log('bump!')
+  }
+}
